923a6b0f7e98a858cff020de7fab2d269e175d61
harish ashwin raj. s <ashwinharishraj@gmail.com>
2021-03-18 18:37:24 +0530

    commit try

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+class App extends Component{
+  render(){
+    return(
+      <div>
+    <div className = "container teal "><p>
+      harish ash raj
+    </p></div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"));
